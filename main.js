e9c6ad109d6c4951164f6e06c913c406102ff50f
rhfiguer <76163244+rhfiguer@users.noreply.github.com>
2021-03-23 10:27:57 +0100

    Update main.js

--- main.js
+++ main.js
@@ -14,28 +14,28 @@ function flip(event){
             "respuesta": "(1) (Yo) me lavo / Ella se lava"
         },
         {
-            "pregunta":"(2) Oversett: Han gir dem mange problemer",
-            "respuesta":"(2) Él les da muchos problemas"
+            "pregunta":"(2) Oversett: Han snakker om mange problemer",
+            "respuesta":"(2) Él habla acerca de/sobre muchos problemas"
         },
         {
-            "pregunta": "(3) Oversett: Jeg spiser din pizza",
-            "respuesta": "(3) (Yo) como tu pizza"
+            "pregunta": "(3) Oversett: Jeg spiser pizza",
+            "respuesta": "(3) (Yo) como pizza"
         },
         {
-            "pregunta": "(4) Oversett: Takk for din pizza",
-            "respuesta": "(4) Gracias por tu pizza"
+            "pregunta": "(4) Oversett: Takk for pizza",
+            "respuesta": "(4) Gracias por la pizza"
         },
         {
-            "pregunta": "(5) Oversett: Vi må gi henne alle bøkene",
-            "respuesta": "(5) (nosotros-as) le tenemos que dar todos los libros"
+            "pregunta": "(5) Oversett: Vi må gi alle bøkene",
+            "respuesta": "(5) (nosotros-as) tenemos que dar todos los libros"
         },
         {
-            "pregunta": "(6) Oversett: Paula skriver han et fint brev",
-            "respuesta": "(6) Paula le escribe una carta linda"
+            "pregunta": "(6) Oversett: Paula skriver et fint brev",
+            "respuesta": "(6) Paula escribe una carta linda"
         },
         {
             "pregunta": "(7) Oversett: Juan føler seg dårlig i dag",
-            "respuesta": "(7) Juan se siente mal el día de hoy"
+            "respuesta": "(7) Juan se siente mal hoy/el día de hoy"
         },
         {
             "pregunta": "(8) Oversett: Hun skal legge seg tidlig",
@@ -46,32 +46,32 @@ function flip(event){
             "respuesta": "(9) Ellos-as van a estudiar Matemáticas mañana"
         },
         {
-            "pregunta": "(10) Oversett: Jeg vil våkne opp kl. 6 i morgen",
-            "respuesta": "(10) (Yo) me quiero levantar a las 6 mañana)"
+            "pregunta": "(10) Oversett: Jeg skal våkne opp kl. 6 i morgen",
+            "respuesta": "(10) (Yo) voy a levantarme a las 6 mañana"
         },
         {
-            "pregunta": "(11) Oversett: Vi liker å gå på kino",
-            "respuesta": "(11) (A nosotros-as) Nos gusta ir al cine"
+            "pregunta": "(11) Oversett: Vi liker å dra på kino",
+            "respuesta": "(11) Nos gusta ir al cine"
         },
         {
             "pregunta": "(12) Oversett: Hun liker å høre musikk",
-            "respuesta": "(12) (A ella) le gusta escuchar música"
+            "respuesta": "(12) Le gusta escuchar música"
         },
         {
             "pregunta": "(13) Oversett: Jeg ser på mye TV om kvelden",
-            "respuesta": "(13) (Yo) veo mucha TV por la noche"
+            "respuesta": "(13) (Yo) veo mucha TV por/en la noche"
         },
         {
             "pregunta": "(14) Oversett: Jeg har reist til Barcelona i sommer",
-            "respuesta": "(14) (Yo) he viajado a Barcelona este verano"
+            "respuesta": "(14) (Yo) he viajado a Barcelona este verano/en el verano"
         },
         {
             "pregunta": "(15) Oversett: Det er sol i dag",
-            "respuesta": "(15) Hay sol hoy == Hace sol hoy"
+            "respuesta": "(15) Hay sol hoy / Hace sol hoy"
         },
         {
             "pregunta": "(16) Oversett: Det skal være sol i dag",
-            "respuesta": "(16) Va a hacer sol hoy"
+            "respuesta": "(16) Va a hacer/haber sol hoy"
         },
         {
             "pregunta": "(17) Oversett: Det har regnet hele dagen",
@@ -82,12 +82,12 @@ function flip(event){
             "respuesta": "(18) Ha hecho calor/frío el verano pasado"
         },
         {
-            "pregunta": "(19) Oversett: Hvem vil komme til mitt hus?",
-            "respuesta": "(19) Quién quiere venir a mi casa"
+            "pregunta": "(19) Oversett: Har du lyst til å komme til mitt hus?",
+            "respuesta": "(19) Quieres venir a mi casa?"
         },
         {
-            "pregunta":"(20) Oversett: Skal vi studere på ditt eller mitt hus",
-            "respuesta":"(20) (Nosotros-as) Vamos a estudiar en tu o en mi casa"
+            "pregunta":"(20) Oversett: Skal vi studere på mitt hus",
+            "respuesta":"(20) (Nosotros-as) Vamos a estudiar en mi casa"
         },
         {
             "pregunta": "(21) Oversett: Så fin er din klokke!",
@@ -111,11 +111,11 @@ function flip(event){
         },
         {
             "pregunta": "(26) Traducir: No quiero comer",
-            "respuesta": "(26) Jeg vil ikke spise "
+            "respuesta": "(26) Jeg har ikke lyst til å spise "
         },
         {
             "pregunta": "(27) Traducir: ¿Hablas ruso? No, no hablo ruso",
-            "respuesta": "(27) Snakker du russisk? Nei, jeg snakker ikke russisk"
+            "respuesta": "(27) Snakker du russisk? Nei, jeg ikke snakker russisk"
         },
         {
             "pregunta": "(28) Traducir: No hay tigres en Noruega",
@@ -142,28 +142,28 @@ function flip(event){
             "respuesta": "(33) Jeg husker deg"
         },
         {
-            "pregunta": "(34) Traducir: Mi madre nos espera",
-            "respuesta": "(34) Min mor venter på oss"
+            "pregunta": "(34) Traducir: Mi madre espera",
+            "respuesta": "(34) Min mor venter på"
         },
         {
-            "pregunta": "(35) Traducir: Esta chica es súper simpática (== simpatiquísima)",
-            "respuesta": "(35) Denne jenta er kjempehyggelig"
+            "pregunta": "(35) Traducir: La chica es súper simpática (== simpatiquísima)",
+            "respuesta": "(35) Den jenta er kjempehyggelig"
         },
         {
-            "pregunta": " (36) Traducir: ¡Estos zapatos son súper caros (==carísimos)",
-            "respuesta": "(36) Disse skoene er kjempedyre!"
+            "pregunta": " (36) Traducir: ¡Los zapatos son súper caros (==carísimos)",
+            "respuesta": "(36) De skoene er kjempedyre!"
         },
         {
-            "pregunta": "(37) Traducir: Esta comida está buenísima (== muy buena)",
-            "respuesta": "(37) Denne maten er kjempegod"
+            "pregunta": "(37) Traducir: La comida está buenísima (== muy buena)",
+            "respuesta": "(37) Den maten er kjempegod"
         },
         {
-            "pregunta": "(38) Traducir: Mi casa es la más bonita",
-            "respuesta": "Mitt hus er det vakreste huset"
+            "pregunta": "(38) Traducir: Mi casa es la casa bonita",
+            "respuesta": "Mitt hus er det vakkre huset"
         },
         {
-            "pregunta": "(39) Traducir: Mi cuaderno es más viejo que tu cuaderno",
-            "respuesta": "(39) Min skrevebok er eldre enn din skrivebok"
+            "pregunta": "(39) Traducir: Mi cuaderno está en la mesa",
+            "respuesta": "(39) Min skrevebok er på boret"
         },
         {
             "pregunta": "(40) Traducir: La casa blanca",
@@ -182,28 +182,28 @@ function flip(event){
             "respuesta": "(43) Hva har du gjort i helga?"
         },
         {
-            "pregunta": "(44) Traducir: Mi papá ha vivido toda su vida en Madrid",
-            "respuesta": "(44) Min pappa har bodd hele livet sitt i Madrid"
+            "pregunta": "(44) Traducir: Mi papá ha vivido toda la vida en Madrid",
+            "respuesta": "(44) Min pappa har bodd hele livet i Madrid"
         },
         {
-            "pregunta": "(45) Traducir: A los 20 años he empezado a trabajar",
-            "respuesta": "(45) Når jeg var 20 år gammel har jeg begynt å jobbe"
+            "pregunta": "(45) Traducir: Yo he empezado a trabajar",
+            "respuesta": "(45) Jeg har begynt å jobbe"
         },
         {
-            "pregunta": "(46) Traducir: La última semana ha sido muy tranquila (==tranquilísima)",
-            "respuesta": "(46) Den forrige uka var veldig rolig"
+            "pregunta": "(46) Traducir: La última semana ha sido/estado muy tranquila (==tranquilísima)",
+            "respuesta": "(46) Den forrige uka har vært veldig rolig"
         },
         {
             "pregunta": "(47) Traducir: Todos los veranos viajamos a Gran Canaria",
             "respuesta": "(47) Alle somrene reiser vi til Gran Canaria"
         },
         {
-            "pregunta": "(48) Traducir: Hace dos años visitamos México",
-            "respuesta": "(48) For to år siden besøker vi Mexico"
+            "pregunta": "(48) Traducir: Vamos a visitar México este año",
+            "respuesta": "(48) Vi skal besøke Mexico dette året"
         },
         {
-            "pregunta": "(49) Traducir: Hace tres días yo he estudiado por última vez",
-            "respuesta": "(49) For tre dager siden jeg har studert på siste gangen"
+            "pregunta": "(49) Traducir: Yo he estudiado mucho para el examen",
+            "respuesta": "(49) Jeg har studert mye på den eksamen"
         },
         {
             "pregunta": "(50) Traducir: Ayer ",
